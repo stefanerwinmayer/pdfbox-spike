6d7dc3c321e284a8f9d3fa9c4b75eb840a36b70c
Stefan E. Mayer <stefan.e.mayer@gmail.com>
2019-10-02 20:42:09 +0100

    WIP

--- src/main/scala/spike/Spike.scala
+++ src/main/scala/spike/Spike.scala
@@ -1,5 +1,11 @@
 package spike
 
+import org.apache.pdfbox.examples.util.PDFMergerExample
+
 object Spike extends App {
-  println("spike")
+  println("Spike starts")
+  val pdfMerger = new PDFMergerExample
+  val listInputStream = ???
+  val inputStream = pdfMerger.merge(listInputStream)
+  println("Spike finished")
 }
