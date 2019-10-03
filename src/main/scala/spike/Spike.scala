46e24844274052c8fcb9dec6c5ef874725c7ce73
Stefan E. Mayer <stefan.mayer@ovoenergy.com>
2019-10-03 12:53:22 +0100

    Add initial version with hard coded pdfs

--- src/main/scala/spike/Spike.scala
+++ src/main/scala/spike/Spike.scala
@@ -1,11 +1,29 @@
 package spike
 
+import java.io.{File, InputStream}
+
+import collection.JavaConverters._
+
+import org.apache.commons.io.FileUtils
 import org.apache.pdfbox.examples.util.PDFMergerExample
 
 object Spike extends App {
+
   println("Spike starts")
-  val pdfMerger = new PDFMergerExample
-  val listInputStream = ???
-  val inputStream = pdfMerger.merge(listInputStream)
+
+  val dummyInputStream =
+    FileUtils
+      .openInputStream(new File("./sample-pdfs/dummy.pdf"))
+      .asInstanceOf[InputStream]
+  val sampleInputStream =
+    FileUtils
+      .openInputStream(new File("./sample-pdfs/sample.pdf"))
+      .asInstanceOf[InputStream]
+  val mergedInputStream = (new PDFMergerExample).merge(
+    List(dummyInputStream, sampleInputStream).asJava
+  )
+
+  FileUtils.copyInputStreamToFile(mergedInputStream, new File("output.pdf"))
+
   println("Spike finished")
 }
