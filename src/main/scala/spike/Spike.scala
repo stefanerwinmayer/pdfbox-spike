3e8cfe7c919aa13b0846935cb75ef07b87d12310
Stefan E. Mayer <stefan.mayer@ovoenergy.com>
2019-10-03 15:50:45 +0100

    Convert to fs2

--- src/main/scala/spike/Spike.scala
+++ src/main/scala/spike/Spike.scala
@@ -4,30 +4,43 @@ import java.io.{File, InputStream}
 
 import collection.JavaConverters._
 
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+import fs2._
 import org.apache.commons.io.FileUtils
 
-object Spike extends App {
+object Spike extends IOApp {
 
-  println("Spike starts")
+  val app: Stream[IO, Unit] =
+    Stream.eval(
+      IO {
+        println("Spike starts")
 
-  val file1 = "dummy.pdf"
-  val pathFile1 = "./samplePDFs/"
-  val file2 = "sample.pdf"
-  val pathFile2 = "./samplePDFs/"
+        val file1 = "dummy.pdf"
+        val pathFile1 = "./samplePDFs/"
+        val file2 = "sample.pdf"
+        val pathFile2 = "./samplePDFs/"
 
-  val dummyInputStream =
-    FileUtils
-      .openInputStream(new File(s"$pathFile1$file1"))
-      .asInstanceOf[InputStream]
-  val sampleInputStream =
-    FileUtils
-      .openInputStream(new File(s"$pathFile2$file2"))
-      .asInstanceOf[InputStream]
-  val mergedInputStream = (new PDFMerger).merge(
-    List(dummyInputStream, sampleInputStream).asJava
-  )
+        val dummyInputStream =
+          FileUtils
+            .openInputStream(new File(s"$pathFile1$file1"))
+            .asInstanceOf[InputStream]
+        val sampleInputStream =
+          FileUtils
+            .openInputStream(new File(s"$pathFile2$file2"))
+            .asInstanceOf[InputStream]
+        val mergedInputStream = (new PDFMerger).merge(
+          List(dummyInputStream, sampleInputStream).asJava
+        )
 
-  FileUtils.copyInputStreamToFile(mergedInputStream, new File("output.pdf"))
+        FileUtils
+          .copyInputStreamToFile(mergedInputStream, new File("output.pdf"))
 
-  println("Spike finished")
+        println("Spike finished")
+      }
+    )
+
+  def run(args: List[String]): IO[ExitCode] =
+    app.compile.drain.as(ExitCode.Success)
 }
